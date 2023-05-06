2fb153bb7d0b635e8b47dc73127798eea337381c
hiro <hirokazu913@gmail.com>
2023-05-07 01:21:12 +0900

    baseURL bugfix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/kazufree33/job-support.git/',
+  base: '/job-support/',
   plugins: [react()],
 });
